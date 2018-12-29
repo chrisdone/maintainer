21a938d5cad333e1bb08d3c2ada7aae533a93e7a
Chris Done <chrisdone@gmail.com>
2018-12-29 23:02:35 +0000

    Display issues

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
@@ -7,9 +8,11 @@ import           Data.Aeson.Types
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Char
 import qualified Data.HashMap.Strict as HM
+import           Data.List
+import           Data.Maybe
+import           Data.Ord
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -23,60 +26,77 @@ data Config = Config
   { configUsername :: ByteString
   , configToken :: ByteString
   , configIgnore :: [Text]
+  , configLimit :: Int
   }
 
 instance FromJSON Config where
   parseJSON j = do
     o <- parseJSON j
     Config <$> fmap S8.pack (o .: "username") <*> fmap S8.pack (o .: "token") <*>
-      (o .: "ignore")
+      (o .: "ignore") <*> o .: "display-limit"
 
 main :: IO ()
 main = do
   config <- decodeFileThrow "maintainer.yaml"
   createDirectoryIfMissing True "data"
-  repos <-
+  reposjson <-
     getCachedResource
       config
       "data/repos.json"
       (\perpage page ->
          "https://api.github.com/user/repos?per_page=" ++
          show perpage ++ "&page=" ++ show page)
+  let repos = mapMaybe (getWantedRepo config) reposjson
+  issues <-
+    fmap
+      (sortBy (comparing (\(_name, _title, _url, String updated) -> updated)) .
+       take (configLimit config) . concat)
+      (mapM
+         (\fullName -> do
+            issues <-
+              getCachedResource
+                config
+                ("data/issues." <>
+                 T.unpack
+                   (T.map
+                      (\c ->
+                         if isAlphaNum c
+                           then c
+                           else '.')
+                      fullName) <>
+                 ".json")
+                (\perpage page ->
+                   "https://api.github.com/repos/" <> T.unpack fullName <>
+                   "/issues?state=open&sort=updated&direction=asc&per_page=" ++
+                   show perpage ++ "&page=" ++ show page)
+            pure
+              (mapMaybe
+                 (\issue ->
+                    (fullName, , , ) <$> HM.lookup "title" issue <*>
+                    HM.lookup "html_url" issue <*>
+                    HM.lookup "updated_at" issue)
+                 issues))
+         repos)
   mapM_
-    (\result ->
-       case HM.lookup "full_name" result of
-         Just (String fullName) ->
-           if any
-                (\ignored ->
-                   if T.isSuffixOf "/" ignored
-                     then T.isPrefixOf ignored fullName
-                     else ignored == fullName)
-                (configIgnore config) ||
-              HM.lookup "archived" result == Just (Bool True) ||
-              HM.lookup "private" result == Just (Bool True)
-             then pure ()
-             else do
-               T.putStrLn fullName
-               issues <-
-                 getCachedResource
-                   config
-                   ("data/issues." <>
-                    T.unpack
-                      (T.map
-                         (\c ->
-                            if isAlphaNum c
-                              then c
-                              else '.')
-                         fullName) <>
-                    ".json")
-                   (\perpage page ->
-                      "https://api.github.com/repos/" <> T.unpack fullName <>
-                      "/issues?state=open&sort=updated&direction=asc&per_page=" ++
-                      show perpage ++ "&page=" ++ show page)
-               -- mapM_ (L8.putStrLn . encode) issues
-               pure ()
-         _ -> pure ())
-    repos
+    (\(_name, String title, String url, String updated) ->
+       T.putStrLn (T.unlines [title, url, updated]))
+    issues
+
+getWantedRepo ::Config -> HM.HashMap Text Value -> Maybe Text
+getWantedRepo config result =
+  case HM.lookup "full_name" result of
+    Just (String fullName) ->
+      if any
+           (\ignored ->
+              if T.isSuffixOf "/" ignored
+                then T.isPrefixOf ignored fullName
+                else ignored == fullName)
+           (configIgnore config) ||
+         HM.lookup "archived" result == Just (Bool True) ||
+         HM.lookup "private" result == Just (Bool True)
+        then Nothing
+        else Just fullName
+    _ -> Nothing
 
 getCachedResource :: Config -> FilePath -> (Int -> Int -> String) -> IO [Object]
 getCachedResource config cachefile mkurl = do
